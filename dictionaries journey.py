4d1afb0f4b076c9dbdb16bb13f42cca92f2b4bbc
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-28 17:44:19 +0200

    initializing company_employees

--- dictionaries journey.py
+++ dictionaries journey.py
@@ -20,6 +20,18 @@
 #         wordNumbsDict[i]=1
 # print(wordNumbsDict)
 
+
+#Third Question
+company_employees = {
+"Engineering": {
+"Alice": {"age": 30, "role": "Software Engineer"},
+"Bob": {"age": 28, "role": "DevOps Engineer"}
+},
+"HR": {
+"Charlie": {"age": 35, "role": "HR Manager"}
+}
+}
+
     
     
 
