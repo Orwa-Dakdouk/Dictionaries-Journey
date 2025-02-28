c2a503b265e70e53d5ae8c7d5fbe2cb353c1e41f
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-28 17:21:02 +0200

    commenting out the first question

--- dictionaries journey.py
+++ dictionaries journey.py
@@ -1,11 +1,12 @@
-def merge_dictionaries(dict1,dict2):
-    mergedDictionary= dict1.copy()
-    for key,value in dict2.items():
-        mergedDictionary[key]=value
-    return mergedDictionary
-dict1 = {"a":1,"b":2,"c":3}
-dict2 = {"b":10,"d":4}
-print(merge_dictionaries(dict1,dict2))
+#First Question
+# def merge_dictionaries(dict1,dict2):
+#     mergedDictionary= dict1.copy()
+#     for key,value in dict2.items():
+#         mergedDictionary[key]=value
+#     return mergedDictionary
+# dict1 = {"a":1,"b":2,"c":3}
+# dict2 = {"b":10,"d":4}
+# print(merge_dictionaries(dict1,dict2))
 
 
 
