19bfa8becf217ffd29788e81bf0396f802b22fb8
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-28 17:10:23 +0200

    defining dict1 and dict2

--- dictionaries journey.py
+++ dictionaries journey.py
@@ -3,5 +3,8 @@ def merge_dictionaries(dict1,dict2):
     for key,value in dict2.items():
         mergedDictionary[key]=value
     return mergedDictionary
+dict1 = {"a":1,"b":2,"c":3}
+dict2 = {"b":10,"d":4}
+
 
 
