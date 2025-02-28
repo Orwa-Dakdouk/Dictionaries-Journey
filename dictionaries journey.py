95dc641de4ef876997dab7144739f79d8245944b
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-28 18:24:32 +0200

    commentint third question

--- dictionaries journey.py
+++ dictionaries journey.py
@@ -22,23 +22,26 @@
 
 
 #Third Question
-company_employees = {
-"Engineering": {
-"Alice": {"age": 30, "role": "Software Engineer"},
-"Bob": {"age": 28, "role": "DevOps Engineer"}
-},
-"HR": {
-"Charlie": {"age": 35, "role": "HR Manager"}
-}
-}
-print(company_employees)
-company_employees["Engineering"]["David"] = {"age":27,"role":"Data Scientist"}
-def countTotalEmployees(company_employees):
-    total = 0
-    for section in company_employees.values():
-        for employee in section:
-            total += 1
-    return total
+# company_employees = {
+# "Engineering": {
+# "Alice": {"age": 30, "role": "Software Engineer"},
+# "Bob": {"age": 28, "role": "DevOps Engineer"}
+# },
+# "HR": {
+# "Charlie": {"age": 35, "role": "HR Manager"}
+# }
+# }
+# print(company_employees)
+# company_employees["Engineering"]["David"] = {"age":27,"role":"Data Scientist"}
+# def countTotalEmployees(company_employees):
+#     total = 0
+#     for section in company_employees.values():
+#         for employee in section:
+#             total += 1
+#     return total
+
+
+
             
         
     
